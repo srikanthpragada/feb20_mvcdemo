9f1142800bb3d345876f7d4d6a74d4905be8c8f4
srikanthpragada <srikanthpragada@gmail.com>
2017-03-22 09:59:02 +0530

    22-MAR

---
+++ Controllers/DiscountController.cs
@@ -0,0 +1,26 @@
+﻿using MvcDemo.Models;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class DiscountController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            DiscountModel model = new DiscountModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(DiscountModel model)
+        {
+            if (ModelState.IsValid)
+                model.Discount = model.Amount * model.Rate / 100;
+            else
+                model.Discount = 0; 
+
+            return View(model);
+        }
+    }
+}
---
+++ Models/DiscountModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcDemo.Models
+{
+    public class DiscountModel
+    {
+        [Range(1000,100000, ErrorMessage = "Amount must be 1000 to 100000")]
+        public double Amount { get; set; }
+
+        [Range(5, 50, ErrorMessage = "Discount rate must be 5 to 50")]
+        public double Rate { get; set; }
+
+        public double Discount { get; set; }
+
+    }
+}
