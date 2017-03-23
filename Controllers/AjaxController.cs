21a0dbf1584a4436f8b509a86b7a6cd6830a86ac
srikanthpragada <srikanthpragada@gmail.com>
2017-03-23 10:01:41 +0530

    23-MAR

---
+++ Controllers/AjaxController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class AjaxController : Controller
+    {
+
+        List<String> books = new List<String> { "C# Comp. Ref", "Asp.Net MVC Unleashed", "AngularJS" };
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Search(string title)
+        {
+            return View();
+        }
+
+
+
+        public String Now()
+        {
+            return DateTime.Now.ToString();
+        }
+
+        public String Title(int id)
+        {
+            if (id <= books.Count)
+                return books[id - 1];
+            else
+                return "";
+        }
+    }
+}
